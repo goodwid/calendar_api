c23e76316015f560247840584b5658987a82a1e4
David <goodwid@gmail.com>
2016-06-01 16:43:36 -0700

    Working /date route

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-const app = require('./app');
+require ('./lib/setup-mongoose');
+const app = require('./lib/app');
+const port = process.env.PORT || process.argv[2] || 9000;
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
--- routes/dates.js
+++ routes/dates.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const m = require('moment');
 
-router.get('/', (req, res) => {
-  res.send(`${req.params.key} shows ${m()}`);
+router.get('/:key', (req, res) => {
+  res.send(`${req.params.key} shows ${m().format('HH:mm')}`);
 });
 
 module.exports = router;
